127923d3c85bdbd71808274ec74dc79556f8f0ff
Debriks <deborah.robbiano@gmail.com>
2022-11-09 14:28:12 +0100

    HTML / CSS / Dropdown Nav / Media Queries

---
+++ script.js
@@ -0,0 +1,4 @@
+const navLinkTitles = document.getElementById("navLinkTitle");
+const btnHamburger = document.getElementById("btnHamburger");
+const btnClose = document.getElementById("btnClose");
+const navBar = document.getElementById("navBar");
