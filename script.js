3c89dfef0023874b822dae4b9eed9e5a99507520
Debriks <deborah.robbiano@gmail.com>
2022-11-09 17:36:26 +0100

    JS / Adjustments in mobile and desktop nav

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-const navLinkTitles = document.getElementById("navLinkTitle");
+const navLinkTitles = document.querySelectorAll(".nav-link-title");
 const btnHamburger = document.getElementById("btnHamburger");
 const btnClose = document.getElementById("btnClose");
 const navBar = document.getElementById("navBar");
+
+const toggleSubNavs = (navLinkTitle, i) => {
+  const subNavLink = navLinkTitle.nextElementSibling;
+  !navLinkTitle.parentElement.classList.contains("open")
+    ? (subNavLink.style.maxHeight = `${subNavLink.scrollHeight + 32}px`)
+    : (subNavLink.style.maxHeight = "0px");
+  navLinkTitles.forEach((_navLinkTitle, _i) => {
+    if (i !== _i) {
+      const subNavLink = _navLinkTitle.nextElementSibling;
+      _navLinkTitle.parentElement.classList.remove("open");
+      if (subNavLink) subNavLink.style.maxHeight = "0px";
+    }
+  });
+  navLinkTitle.parentElement.classList.toggle("open");
+};
+
+navLinkTitles.forEach((navLinkTitle, i) => {
+  navLinkTitle.addEventListener("click", () => {
+    toggleSubNavs(navLinkTitle, i);
+  });
+});
+
+btnHamburger.addEventListener("click", () => navBar.classList.add("open"));
+btnClose.addEventListener("click", () => navBar.classList.remove("open"));
